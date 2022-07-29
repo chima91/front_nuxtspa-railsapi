21d877ca37c1f80207e82e9493f0f2790b5b93dd
web-novice-tech <itb4769we@gmail.com>
2022-07-29 13:51:22 +0900

    会員登録フォームの親子コンポーネント間における双方向データバインディングの実装

--- pages/signup.vue
+++ pages/signup.vue
@@ -2,13 +2,14 @@
   <user-form-card>
     <template #user-form-card-content>
       <v-form v-model="isValid">
-        <user-form-name />
-        <user-form-email />
-        <user-form-password />
+        <user-form-name :name.sync="params.user.name" />
+        <user-form-email :name.sync="params.user.email" />
+        <user-form-password :name.sync="params.user.password" />
         <v-btn :disabled="!isValid" block class="white--text" color="appblue">
           登録
         </v-btn>
       </v-form>
+      {{ params }}
     </template>
   </user-form-card>
 </template>
@@ -19,6 +20,7 @@ export default {
   data() {
     return {
       isValid: false,
+      params: { user: { name: '', email: '', password: '' } }
     }
   },
 }
