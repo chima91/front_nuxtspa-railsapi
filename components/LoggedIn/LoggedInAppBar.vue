8bc33014f593cdd488572d1fbc8ed6a81302a8c9
web-novice-tech <itb4769we@gmail.com>
2022-08-02 08:48:32 +0900

    pages/projectsにプロジェクト一覧を表示

--- components/LoggedIn/LoggedInAppBar.vue
+++ components/LoggedIn/LoggedInAppBar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app-bar app dense elevation="1" color="white">
-    <nuxt-link :to="redirectPath" class="text-decoration-none">
+    <nuxt-link :to="homePath" class="text-decoration-none">
       <app-logo />
     </nuxt-link>
     <app-title />
@@ -15,7 +15,7 @@
   export default {
     data ({ $store }) {
       return {
-        redirectPath: $store.state.loggedIn.redirectPath
+        homePath: $store.state.loggedIn.homePath
       }
     }
   }
