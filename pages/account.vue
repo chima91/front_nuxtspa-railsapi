1f35b4cce1b4046e37f714ca3a04196fa7f06a0e
web-novice-tech <itb4769we@gmail.com>
2022-08-11 14:36:37 +0900

    middlewareを使った現在のプロジェクト番号の取得&更新を実装

--- pages/account.vue
+++ pages/account.vue
@@ -1,14 +1,34 @@
 <template>
   <v-container>
+    <v-btn
+      v-if="!!currentProject"
+      small
+      plain
+      color="primary"
+      :to="$my.projectLinkTo(currentProject.id, dashboardPath)"
+    >
+      <v-icon>mdi-chevron-double-left</v-icon>
+      {{ $my.pageTitle(dashboardPath) }}に戻る
+    </v-btn>
     <nuxt-child />
   </v-container>
 </template>
 
 <script>
-  export default {
-    layout: 'logged-in',
-    validate({ route }) {
-      return route.name !== 'account'
+export default {
+  layout: "logged-in",
+  validate({ route }) {
+    return route.name !== "account"
+  },
+  data() {
+    return {
+      dashboardPath: "project-id-dashboard",
     }
-  }
+  },
+  computed: {
+    currentProject() {
+      return this.$store.state.project.current
+    },
+  },
+}
 </script>
